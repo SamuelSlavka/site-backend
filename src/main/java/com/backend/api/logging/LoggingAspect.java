53fd4a610601a2315ca4cfbed5ad62108f7a52f8
SamuelSlavka <slavka.samuel@gmail.com>
2023-07-22 12:20:55 +0200

    Added auth to services and controllers

--- src/main/java/com/backend/api/logging/LoggingAspect.java
+++ src/main/java/com/backend/api/logging/LoggingAspect.java
@@ -1,23 +1,27 @@
 package com.backend.api.logging;
 
+import com.backend.api.wiki.controller.ArticleController;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.JoinPoint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
+    private final Logger logger = LoggerFactory.getLogger((ArticleController.class));
 
     @Before("execution(* com.backend.api.wiki.service.ArticleService.createArticle(..))")
     public void beforeLogger(JoinPoint jp) {
         String arg = jp.getArgs()[0].toString();
-        System.out.println("Article creation started " + arg);
+        logger.debug("Article creation started {}", arg);
     }
 
     @After("execution(* com.backend.api.wiki.service.ArticleService.createArticle(..))")
     public void afterLogger() {
-        System.out.println("Article created");
+        logger.debug("Article created");
     }
 }
